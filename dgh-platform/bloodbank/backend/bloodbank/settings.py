3c4355facd662fdb31b978cd660879c6d5b817bb
Ackerman-2k22 <franck.nyawa@2026.ucac-icam.com>
2025-08-02 00:54:30 +0100

    deploy

--- dgh-platform/bloodbank/backend/bloodbank/settings.py
+++ dgh-platform/bloodbank/backend/bloodbank/settings.py
@@ -37,7 +37,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://localhost:3000",
-    "https://high-5-blood2care.vercel.app/"
+    "https://high-5-blood2care.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
